24ab7a4234d7f3fd887905c84f557fe6389f8553
BUserE <user@Users-MacBook-Air.local>
2024-11-04 15:15:12 -0600

    more updates

--- Desktop/SeleniumProject/src/test/java/testNG/TestNGclass.java
+++ Desktop/SeleniumProject/src/test/java/testNG/TestNGclass.java
@@ -2,6 +2,17 @@ package testNG;
 
 public class TestNGclass {
     public static void main(String[] args) {
-        System.out.println("Hi");
+      reverse("null");
+
+    }
+
+    public static void reverse(String text){
+        if(text == null){
+            System.out.println("Text is null");
+        }
+
+        for (int i = text.length()-1;  i >=0 ; i--) {
+            System.out.println(text.charAt(i));
+        }
     }
 }
